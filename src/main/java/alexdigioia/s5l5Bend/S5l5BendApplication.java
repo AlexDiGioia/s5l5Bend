561a849d01021ab4260a29abb6d45a6201e2f17c
AlexDiGioia <digioia.alex2001@gmail.com>
2024-09-06 09:10:17 +0200

    Initial commit

---
+++ src/main/java/alexdigioia/s5l5Bend/S5l5BendApplication.java
@@ -0,0 +1,13 @@
+package alexdigioia.s5l5Bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5l5BendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5l5BendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexdigioia/s5l5Bend/S5l5BendApplicationTests.java
@@ -0,0 +1,13 @@
+package alexdigioia.s5l5Bend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5l5BendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
